8d6aaee8585eba3edae14ed7ff0513547c603adb
心飞为你飞 <747011882@qq.com>
2023-03-21 23:35:53 +0800

    解决控制台错误

--- hitch-account/src/main/java/com/heima/account/configuration/BaiduAPIConfiguration.java
+++ hitch-account/src/main/java/com/heima/account/configuration/BaiduAPIConfiguration.java
@@ -1,6 +1,7 @@
 package com.heima.account.configuration;
 
 import com.heima.commons.ai.BaiduAIHelper;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,9 +16,8 @@ public class BaiduAPIConfiguration {
     @Value("${baidu.secretkey}")
     private String secretKey;
 
-
     @Bean
-    public BaiduAIHelper baiduAIHelper(StringRedisTemplate redisTemplate) {
+    public BaiduAIHelper baiduAIHelper(@Qualifier("stringRedisTemplate") StringRedisTemplate redisTemplate) {
         return new BaiduAIHelper(apiKey, secretKey, redisTemplate);
     }
 
--- hitch-gateway/src/main/java/com/heima/gateway/GatewayApplication.java
+++ hitch-gateway/src/main/java/com/heima/gateway/GatewayApplication.java
@@ -3,10 +3,12 @@ package com.heima.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 服务网关
+ */
 @SpringBootApplication
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
-
     }
 }
